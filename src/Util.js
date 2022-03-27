7161b2989fcf477d60856b61a96ef31a7fa34f6f
Imre Mehesz <imehesz@gmail.com>
2022-03-26 23:09:23 -0400

    Started write-aid with Svelte

---
+++ src/Util.js
@@ -0,0 +1,41 @@
+/**
+ * Util.js
+ */
+const Util = {
+    ArrayUtil: {
+        shuffle: (arr) => {
+            var currentIndex = arr.length,  randomIndex;
+
+            while (currentIndex != 0) {
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
+
+                // And swap it with the current element.
+                [arr[currentIndex], arr[randomIndex]] = [
+                arr[randomIndex], arr[currentIndex]];
+            }
+
+            return arr;
+        }
+    },
+
+    ServiceUtil: {
+        loadData: async () => {
+            try {
+                let response = await fetch(MHX.dataUrl)
+
+                if(response.status == 200) {
+                    let json = await response.json()
+                    return json
+                }
+
+                throw new Error("meh, something went wrong", response.status)
+            } catch(err) {
+                console.error(err) // TypeError: failed to fetch
+            }
+        }
+    }
+}
+
+module.exports = Util
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import App from './App.pug';
+
+const app = new App({
+	target: document.body
+});
+
+export default app;
